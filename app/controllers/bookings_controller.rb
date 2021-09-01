ef565155dd708d8141802cfb63167ed341ba1e48
Cesar Ades <kito@ades.as>
2021-09-01 14:40:37 +0100

    added pending to show page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,11 +4,14 @@ class BookingsController < ApplicationController
     @event = Event.find(params[:event_id])
     @booking.user = current_user
     @booking.event = @event
-    redirect_to event_path(@event), notice: "You've registered for this event!" if @booking.save
+    @booking.pending = true
+    redirect_to event_path(@event) if @booking.save
   end
 
   def destroy
-    @booking.destroy
-    redirect_to :back
+    @booking = Booking.find(params[:id])
+    @event = Event.find(params[:event_id])
+    @booking.delete
+    redirect_to event_path(@event), alert: "You've unregistered for this event"
   end
 end
